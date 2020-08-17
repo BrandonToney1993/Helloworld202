59e8c6293901c829195be347c89885fcdcaaad30
Brandon Toney <Bam_toney@aol.com>
2020-08-17 16:24:24 -0500

    added players health
    added players health and regeneration and lvl stats

--- Game.cs
+++ Game.cs
@@ -8,7 +8,18 @@ namespace HelloWorld
     {
         public void Run()
         {
-
+            float health = 100.0f;
+            float healthregen = 80;
+            string name = Console.ReadLine();
+            bool maxLevelReached = false;
+            int maxLevel = 100;
+            int level = 1;
+            bool ready = true;
+            Console.WriteLine(health);
+            health = health + healthregen;
+            Console.Write(name + " you healed " + healthregen);
+            
+            
         }
     }
 }
