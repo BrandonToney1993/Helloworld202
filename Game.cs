4dbd9ae3aa5a0908b95cce71e243bd9e62b707cb
brandontoney1993 <bam_toney@aol.com>
2020-08-19 15:42:35 -0500

    multiple choice questions
    adding in multiple choice questions for multiple answers for character interaction

--- Game.cs
+++ Game.cs
@@ -17,14 +17,34 @@ namespace HelloWorld
             Console.ReadLine();
             Console.WriteLine("how is it that you found me " + name + "?");
             Console.ReadLine();
-            Console.WriteLine("i see so they've sent some child to find bring me in?");
+            Console.WriteLine("i see so they've sent some child to bring me in?");
             Console.ReadLine();
             Console.WriteLine("my name is merlin the great, so the king is in need of my assistence is he " + name + "?");
             Console.ReadLine();
             Console.WriteLine("I can assure you i won't be of any trouble.");
             Console.WriteLine("Please, allow me to heal you before you bring me back to the kingdom");
-            Console.WriteLine("so what are you exactly? you're not like anything ive ever seen before!" + "please input role");
-            string role = Console.ReadLine();
+            Console.WriteLine("so what are you exactly? you're not like anything ive ever seen before!");
+            string role = ("");
+            //ask player to choose a role
+            Console.WriteLine("1.Warrior Role");
+            Console.WriteLine("2.Wizard Role");
+            char Input = Console.ReadKey().KeyChar; 
+            if(Input == '1')
+            {
+                int Attack = 150;
+                int damage = 100;
+                int mana = 20;
+                int warrior;
+               
+            }
+            else if(Input == '2')
+            {
+                int Attack = 100;
+                int Damage = 70;
+                int mana = 150;
+                int wizard;
+                
+            }
             //this variable is used to store health
             float health = 100.0f;
             //this variable is used to heal the player
@@ -33,18 +53,18 @@ namespace HelloWorld
             float magic = 100.0f;
             //this variable is used for player magic regen
             float magicregen = 80.0f;
+            //represent players max level reached
             bool maxLevelReached = false;
+            //represents players max level
             int maxLevel = 100;
+            //represents players level
             int level = 1;
-            bool ready = true;
+            bool Ready = true;
+            //print players stats on screen
             Console.WriteLine("Player Name:" + name);
             Console.WriteLine("Player Health:" + health);
             Console.WriteLine("Player Level:" + level);
             Console.WriteLine("Player Role:" + role);
-            Console.WriteLine("player magic:" + magic);
-            //Create an application that gets the user's name
-            //along with an additional application that gets the user's name
-            //to the console along with their stats. Must use all data types  
 
             
             
