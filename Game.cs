727313a7d2863c0d696efe9c914961069274f68e
brandontoney1993 <bam_toney@aol.com>
2020-08-21 10:23:41 -0500

    while loop

--- Game.cs
+++ Game.cs
@@ -9,14 +9,28 @@ namespace HelloWorld
         public void Run()
         {
             Console.WriteLine("you've been sent out to find a powerful mage to help in the efforts of curing the queen who has been cursed by an evil wizard.");
+
             Console.WriteLine("King Dannahiem has given you the location as to where Merlin has been hiding. The king has given you an order to bring Merlin back by any means necessary.");
-            Console.WriteLine("during your travels, you come across a cave for shelter. once you enter you're met by a hermit, who has fallen. you help him to his feet");
-            Console.WriteLine("Thank you my good boy! my name is merlin, what is yours?");
-            string name = Console.ReadLine();
-            Console.WriteLine("what is it that you seek " + name + "?");
-            Console.WriteLine("1.merlin");
-            Console.WriteLine("2.a battle");
+
+            Console.WriteLine("during your travels, you come across a cave for shelter. once you enter you're met by a hermit, who has fallen.");
+            Console.WriteLine("1.Help the hermit up.");
+            Console.WriteLine("2.keep walking you have more important things to worry about");
             char input = Console.ReadKey().KeyChar;
+            if(input == '1')
+            {
+                Console.WriteLine("thank you my good boy! For helping me, i would like to return the favor by helping you in your travels!");
+            }
+            else if(input == '2')
+            {
+                Console.WriteLine("you try to walk away, but the cries from the old hermit make you return to help.");
+            }
+            Console.WriteLine("what is your name my boy? ");
+
+            string name = Console.ReadLine();
+            Console.WriteLine("what is it that you seek? " + name + "?");
+            Console.WriteLine("1.I seek merlin");
+            Console.WriteLine("2.I seek a battle");
+            char discussion = Console.ReadKey().KeyChar;
             if(input == '1')
             {
                 Console.WriteLine(" well you're in luck you've found me");
@@ -25,36 +39,100 @@ namespace HelloWorld
             {
                 Console.WriteLine(" oh a battle is what you want huh?");
             }
+            
             Console.WriteLine("how is it that you found me " + name + "?");
-            Console.ReadLine();
-            Console.WriteLine("i see so they've sent some child to bring me in?");
-            Console.ReadLine();
-            Console.WriteLine("my name is merlin the great, so the king is in need of my assistence is he " + name + "?");
-            Console.ReadLine();
-            Console.WriteLine("I can assure you i won't be of any trouble.");
-            Console.WriteLine("Please, allow me to heal you before you bring me back to the kingdom");
+            Console.WriteLine("1.the king has sent me to find you, the queen has been cursed!");
+            Console.WriteLine("2.it was a just a conincidence.");
+            char dialog = Console.ReadKey().KeyChar;
+            if(input == '1')
+            {
+                Console.WriteLine("i see so how should i help?");
+            }
+            else if(input == '2')
+            {
+                Console.WriteLine("there are no coincidences my boy!");
+            }
+            else 
+            {
+                Console.WriteLine("invalid input");
+            }
+            
+            Console.WriteLine("1.come back to Dannahiem with me and lift the curse from the queen!");
+            Console.WriteLine("2.help me slay the wizard who cursed the queen!");
+            char convo = Console.ReadKey().KeyChar;
+            if(input == '1')
+            {
+                Console.WriteLine("certainlly my boy, ill do what i can to help!");
+            }
+            else if(input == '2')
+            {
+                Console.WriteLine("Hahahaha....ohhohohoh....thats a good one me fight!");
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+            Console.WriteLine("Please, allow me to heal you before you bring me back to the kingdom.");
+            Console.WriteLine("1.you allow merlin to heal you.");
+            Console.WriteLine("2.you refuse and head back to the kingdom.");
+            char dialect = Console.ReadKey().KeyChar;
+            if(input == '1')
+            {
+                Console.WriteLine("marvelous i'll get right to it my good boy!");
+            }
+            else if(input == '2')
+            {
+                Console.WriteLine("you take merlin by the arm and hoist him up on your horse and quickly ride back to the kingdom of Dannahiem");
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
             Console.WriteLine("so what are you exactly? you're not like anything ive ever seen before!");
             string role = ("");
             //ask player to choose a role
             Console.WriteLine("1.Warrior Role");
             Console.WriteLine("2.Wizard Role");
-            char Input = Console.ReadKey().KeyChar; 
-            if(Input == '1')
+            while (input != '1' && input != '2')
             {
-                int Attack = 150;
-                int damage = 100;
-                int mana = 20;
-                int warrior;
-               
+                char Input = Console.ReadKey().KeyChar;
+                if (Input == '1')
+                {
+                    int Attack = 150;
+                    int warrior;
+
+                }
+                else if (Input == '2')
+                {
+                    int Attack = 100;
+                    int Damage = 70;
+                    int mana = 150;
+                    int wizard;
+
+                }
+                else
+                {
+                    Console.Write("invalid input");
+                }
             }
-            else if(Input == '2')
+
+            Console.WriteLine("+ name + and merlin, are returning to Dannahiem to try and lift the curse placed by the evil wizard. when you're stopped by a merchant.");
+            Console.WriteLine("well hello there my good friends! can i interest you in some antiqueties?");
+            Console.WriteLine("1.we haven't the time you pesant! no move!");
+            Console.WriteLine("2.move or ill move you myself worm!");
+            char converse = Console.ReadKey().KeyChar;
+            if(input == '1')
             {
-                int Attack = 100;
-                int Damage = 70;
-                int mana = 150;
-                int wizard;
-                
+                Console.WriteLine("pesant haha! as you wish sir!");
             }
+            else if(input == '2')
+            {
+                Console.WriteLine("yes my good sir!");
+            }
+
+            Console.WriteLine("as + name + walk away the seemingly easy going merchant transforms into the evil wizard");
+
+            Console.WriteLine("hahaha fools they'll try and lift the curse, but only fail and kill the queen! hahaha");
             //this variable is used to store health
             float health = 100.0f;
             //this variable is used to heal the player
